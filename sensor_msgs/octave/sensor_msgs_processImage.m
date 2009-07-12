fce2b7c9a6c4617094de7f7c42c9c20ce188381f
rdiankov <rdiankov>
2009-07-12 22:10:54 +0000

    sensor_msgs_processImage can now process empty images

--- sensor_msgs/octave/sensor_msgs_processImage.m
+++ sensor_msgs/octave/sensor_msgs_processImage.m
@@ -4,11 +4,17 @@
 function I = sensor_msgs_processImage(image_msg)
 %res.camimage_msg.layout
 %I = zeros([data.height data.width 3]);
+I = [];
+if( isempty(image_msg.depth) )
+    return;
+end
 layout = eval(sprintf('image_msg.%s_data.layout',image_msg.depth));
 dimsizes = [];
 for i = 1:length(layout.dim)
     dimsizes(i) = layout.dim{i}.size;
 end
-I = reshape(eval(sprintf('image_msg.%s_data.data',image_msg.depth)),dimsizes(end:-1:1));
-%% reverse the dimension order
-I = permute(I,length(dimsizes):-1:1);
+if( ~isempty(dimsizes) )
+    I = reshape(eval(sprintf('image_msg.%s_data.data',image_msg.depth)),dimsizes(end:-1:1));
+    %% reverse the dimension order
+    I = permute(I,length(dimsizes):-1:1);
+end
