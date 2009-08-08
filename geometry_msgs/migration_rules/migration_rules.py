99a94bcad74de58bbb4adc5011a57879c5310673
jleibs <jleibs>
2009-08-08 04:00:17 +0000

    Changing migration rule for Twist to go to TwistStamped.

--- geometry_msgs/migration_rules/migration_rules.py
+++ geometry_msgs/migration_rules/migration_rules.py
@@ -24,56 +24,6 @@ float64 z
 		new_msg.z = old_msg.z
 
 
-class update_robot_msgs_Twist_104c6ef591961bed596cfa30f858271c(MessageUpdateRule):
-	old_type = "robot_msgs/Twist"
-	old_full_text = """
-Header header
-robot_msgs/Vector3  vel
-robot_msgs/Vector3  rot
-
-================================================================================
-MSG: roslib/Header
-#Standard metadata for higher-level flow data types
-#sequence ID: consecutively increasing ID 
-uint32 seq
-#Two-integer timestamp that is expressed as:
-# * stamp.secs: seconds (stamp_secs) since epoch
-# * stamp.nsecs: nanoseconds since stamp_secs
-# time-handling sugar is provided by the client library
-time stamp
-#Frame this data is associated with
-# 0: no frame
-# 1: global frame
-string frame_id
-
-================================================================================
-MSG: robot_msgs/Vector3
-float64 x
-float64 y
-float64 z
-"""
-
-	new_type = "geometry_msgs/Twist"
-	new_full_text = """
-Vector3  linear
-Vector3  angular
-
-================================================================================
-MSG: geometry_msgs/Vector3
-float64 x
-float64 y
-float64 z
-"""
-
-	order = 0
-	migrated_types = [('Vector3', 'Vector3')]
-
-	valid = True
-
-	def update(self, old_msg, new_msg):
-                self.migrate(old_msg.vel, new_msg.linear)
-                self.migrate(old_msg.rot, new_msg.angular)
-
 class update_robot_msgs_Quaternion_a779879fadf0160734f906b8c19c7004(MessageUpdateRule):
 	old_type = "robot_msgs/Quaternion"
 	old_full_text = """
@@ -137,7 +87,7 @@ float64 w
 	new_type = "geometry_msgs/QuaternionStamped"
 	new_full_text = """
 Header header
-Quaternion quaternion
+Quaternion data
 
 ================================================================================
 MSG: roslib/Header
@@ -172,7 +122,7 @@ float64 w
 
 	def update(self, old_msg, new_msg):
 		self.migrate(old_msg.header, new_msg.header)
-		self.migrate(old_msg.quaternion, new_msg.quaternion)
+		self.migrate(old_msg.quaternion, new_msg.data)
 
 class update_robot_msgs_Vector3Stamped_7b324c7325e683bf02a9b14b01090ec7(MessageUpdateRule):
 	old_type = "robot_msgs/Vector3Stamped"
@@ -667,3 +617,76 @@ float32 z
 		new_msg.y = old_msg.y
 		new_msg.z = old_msg.z
 
+class update_robot_msgs_Twist_104c6ef591961bed596cfa30f858271c(MessageUpdateRule):
+	old_type = "robot_msgs/Twist"
+	old_full_text = """
+Header header
+robot_msgs/Vector3  vel
+robot_msgs/Vector3  rot
+
+================================================================================
+MSG: roslib/Header
+#Standard metadata for higher-level flow data types
+#sequence ID: consecutively increasing ID 
+uint32 seq
+#Two-integer timestamp that is expressed as:
+# * stamp.secs: seconds (stamp_secs) since epoch
+# * stamp.nsecs: nanoseconds since stamp_secs
+# time-handling sugar is provided by the client library
+time stamp
+#Frame this data is associated with
+# 0: no frame
+# 1: global frame
+string frame_id
+
+================================================================================
+MSG: robot_msgs/Vector3
+float64 x
+float64 y
+float64 z
+"""
+
+	new_type = "geometry_msgs/TwistStamped"
+	new_full_text = """
+Header header
+Twist data
+
+================================================================================
+MSG: roslib/Header
+#Standard metadata for higher-level flow data types
+#sequence ID: consecutively increasing ID 
+uint32 seq
+#Two-integer timestamp that is expressed as:
+# * stamp.secs: seconds (stamp_secs) since epoch
+# * stamp.nsecs: nanoseconds since stamp_secs
+# time-handling sugar is provided by the client library
+time stamp
+#Frame this data is associated with
+# 0: no frame
+# 1: global frame
+string frame_id
+
+================================================================================
+MSG: geometry_msgs/Twist
+Vector3  linear
+Vector3  angular
+
+================================================================================
+MSG: geometry_msgs/Vector3
+float64 x
+float64 y
+float64 z
+"""
+
+	order = 0
+	migrated_types = [
+		("Header","Header"),
+                ("Vector3","Vector3"),]
+
+	valid = True
+
+	def update(self, old_msg, new_msg):
+		self.migrate(old_msg.header, new_msg.header)
+		new_msg.data = self.get_new_class('Twist')()
+                self.migrate(old_msg.vel, new_msg.data.linear)
+                self.migrate(old_msg.rot, new_msg.data.angular)
