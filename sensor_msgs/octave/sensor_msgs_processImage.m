76727b3363d22c2f6e1bf83a9c3be5c55445698c
rdiankov <rdiankov>
2009-09-21 03:26:11 +0000

    fixed octave parsing of image msg

--- sensor_msgs/octave/sensor_msgs_processImage.m
+++ sensor_msgs/octave/sensor_msgs_processImage.m
@@ -2,19 +2,6 @@
 %%
 %% returns an image matrix given a sensor_msgs/Image
 function I = sensor_msgs_processImage(image_msg)
-%res.camimage_msg.layout
-%I = zeros([data.height data.width 3]);
-I = [];
-if( isempty(image_msg.depth) )
-    return;
-end
-layout = eval(sprintf('image_msg.%s_data.layout',image_msg.depth));
-dimsizes = [];
-for i = 1:length(layout.dim)
-    dimsizes(i) = layout.dim{i}.size;
-end
-if( ~isempty(dimsizes) )
-    I = reshape(eval(sprintf('image_msg.%s_data.data',image_msg.depth)),dimsizes(end:-1:1));
-    %% reverse the dimension order
-    I = permute(I,length(dimsizes):-1:1);
-end
+channels = image_msg.encoding
+I = reshape(image_msg.data,[image_msg.step image_msg.height]);
+I = permute(reshape(I(1:channels*image_msg.width,:),[channels image_msg.width image_msg.height]),[3 2 1]);
