fa64844999e8d8377d39913f97d8e226b16c8680
DavidTorresOcana <david.torres.ocana@gmail.com>
2019-10-27 18:38:04 +0100

    Add FISHEYE model definition

--- sensor_msgs/include/sensor_msgs/distortion_models.h
+++ sensor_msgs/include/sensor_msgs/distortion_models.h
@@ -45,6 +45,7 @@ namespace sensor_msgs
     const std::string PLUMB_BOB = "plumb_bob";
     const std::string RATIONAL_POLYNOMIAL = "rational_polynomial";
     const std::string EQUIDISTANT = "equidistant";
+    const std::string FISHEYE = "fisheye";
   }
 }
 
